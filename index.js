cb876ff4cb048e8a4362a76c641342bc9d203953
Gleb Mikheev <mikheev@betaagency.ru>
2015-12-01 18:05:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function createCache(fn) {
+  var value = {};
+  return function() {
+    var index = JSON.stringify( Array.prototype.slice.call(arguments))
+    if (value[index] === undefined) {
+      value[index] = fn.apply(this, arguments);
+    }
+    return value[index];
+  }
+}
