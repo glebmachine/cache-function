5df96342d31a37432e349de38642224f0a533bff
Gleb Mikheev <mikheev@betaagency.ru>
2015-12-01 18:07:02 +0300

    jshint

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function createCache(fn) {
   var value = {};
   return function() {
-    var index = JSON.stringify( Array.prototype.slice.call(arguments))
+    var index = JSON.stringify( Array.prototype.slice.call(arguments));
     if (value[index] === undefined) {
       value[index] = fn.apply(this, arguments);
     }
     return value[index];
-  }
+  };
 }
